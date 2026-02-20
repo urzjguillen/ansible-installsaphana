c45bfd0e9ecb4665cf958ebcb679d6a836c84951
Jose R Guillen <jrguillennavas@gmail.com>
2026-02-20 16:45:10 -0400

    first test to jenkins

--- script/main.py
+++ script/main.py
@@ -7,7 +7,7 @@ try:
             line_strip = line.strip()
             if line_strip.startswith("ansible_host"):
                 ip = line_strip.split(" ")[1]
-                result = subprocess.run(["ssh-keyscan", "-H" f"{ip}", ">>", "~/.ssh/known_hosts"], capture_output=True, text=True)
+                result = subprocess.run(["ssh-keyscan", "-H", f"{ip}", ">>", "~/.ssh/known_hosts"], capture_output=True, text=True)
             continue
 except:
     print("Arvhivo no existe")
